054bd5b9b066f2c2c8afd775d789ebd2cf224b6f
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-12-18 19:51:45 +0530

    loops in java

---
+++ loops/For.java
@@ -0,0 +1,14 @@
+
+public class For {
+
+    public static void main(String[] args) {
+
+        int num = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(num * i);
+            System.out.print(" ");
+
+        }
+    }
+
+}
